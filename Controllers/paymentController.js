569ee72481b90f39b90898936a33d7901201a7b0
stanli206 <stantilin@gmail.com>
2025-05-22 19:22:45 +0530

    `Refactored createCheckoutSession function in paymentController.js`

--- Controllers/paymentController.js
+++ Controllers/paymentController.js
@@ -4,41 +4,38 @@ import dotenv from "dotenv";
 dotenv.config();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-export const createCheckoutSession = async (req, res) => { 
-    try {
-        const { items } = req.body;
+export const createCheckoutSession = async (req, res) => {
+  try {
+    const { items } = req.body;
 
-        console.log("Received items:", items);
-        
-        
-        const line_items = items.map((item) => {
-            if (!item.product || !item.product.price) {
-                throw new Error("Invalid product data - Missing product or price");
-            }
+    console.log("Received items:", items);
 
-            return {
-                price_data: {
-                    currency: "usd",
-                    product_data: { name: item.product.name },
-                    unit_amount: Math.round(item.product.price * 100),
-                },
-                quantity: item.quantity,
-            }
-        });
+    const line_items = items.map((item) => {
+      if (!item.product || !item.product.price) {
+        throw new Error("Invalid product data - Missing product or price");
+      }
 
-        const session = await stripe.checkout.sessions.create({
-            payment_method_types: ["card"],
-            line_items,
-            mode: "payment",
-            success_url: "http://localhost:5173/success",
-            cancel_url: "http://localhost:5173/cancel",
-        })
+      return {
+        price_data: {
+          currency: "usd",
+          product_data: { name: item.product.name },
+          unit_amount: Math.round(item.product.price * 100),
+        },
+        quantity: item.quantity,
+      };
+    });
 
-        res.json({ url: session.url });
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items,
+      mode: "payment",
+      success_url: "https://onlineekart.netlify.app/success",
+      cancel_url: "https://onlineekart.netlify.app/cancel",
+    });
 
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).json({ message: error.message });
-        
-    }
+    res.json({ url: session.url });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: error.message });
+  }
 };
